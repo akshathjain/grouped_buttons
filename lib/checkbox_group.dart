e52ca5c4f8b999bb8505aad25e9d5e3803f67a32
Akshath Jain <akshath.r.jain@gmail.com>
2019-03-15 23:02:35 -0400

    created very basic CheckboxGroup

--- lib/checkbox_group.dart
+++ lib/checkbox_group.dart
@@ -6,16 +6,63 @@ Purpose: define the CheckboxGroup object
 
 part of 'grouped_buttons.dart';
 
-class CheckBoxGroup extends StatefulWidget {
+class CheckboxGroup extends StatefulWidget {
+  final List<String> labels;
+  final Widget Function(bool isChecked, String label, int index) onChange;
+
+  CheckboxGroup({
+    Key key,
+    this.labels,
+    this.onChange,
+  }) : super(key: key);
+
+
   @override
-  _CheckBoxGroupState createState() => _CheckBoxGroupState();
+  _CheckboxGroupState createState() => _CheckboxGroupState();
 }
 
-class _CheckBoxGroupState extends State<CheckBoxGroup> {
+class _CheckboxGroupState extends State<CheckboxGroup> {
+  List<String> _selected = [];
+
   @override
   Widget build(BuildContext context) {
+    
+    List<Widget> content = [];
+
+    for(int i = 0; i < widget.labels.length; i++){
+      content.add(Row(children: <Widget>[
+        Checkbox(
+          value: _selected.contains(widget.labels.elementAt(i)),
+          onChanged: (bool isChecked) => onChanged(isChecked, i),
+        ),
+
+        SizedBox(width: 24.0),
+
+        Text(widget.labels.elementAt(i))
+      ]));
+    }
+
     return Container(
-      
+      child: Column(
+        children: content,
+      ),
     );
   }
+
+  void onChanged(bool isChecked, int i){
+    bool isAlreadyContained = _selected.contains(widget.labels.elementAt(i));
+
+    if(mounted){
+      setState(() {
+        if(!isChecked && isAlreadyContained){
+          _selected.remove(widget.labels.elementAt(i));
+        }else if(isChecked && !isAlreadyContained){
+          _selected.add(widget.labels.elementAt(i));
+        }
+
+        widget.onChange(isChecked, widget.labels.elementAt(i), i);
+      });
+    }
+  }
+
 }
