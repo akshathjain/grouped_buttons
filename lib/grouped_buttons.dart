dd6de7850ee338dc26f60abc88e1104911034b21
Akshath Jain <akshath.r.jain@gmail.com>
2019-03-17 00:42:33 -0400

    added copyright and licensing info to all source code files

--- lib/grouped_buttons.dart
+++ lib/grouped_buttons.dart
@@ -2,6 +2,8 @@
 Name: Akshath Jain
 Date: 3/15/19
 Purpose: defines the package: grouped_buttons
+Copyright: © 2019, Akshath Jain. All rights reserved.
+Licensing: More information can be found here: https://github.com/akshathjain/grouped_buttons/blob/master/LICENSE
 */
 
 library grouped_buttons;
--- lib/radio_button_group.dart
+++ lib/radio_button_group.dart
@@ -2,6 +2,8 @@
 Name: Akshath Jain
 Date: 3/15/19
 Purpose: define the RadioButtonGroup object
+Copyright: © 2019, Akshath Jain. All rights reserved.
+Licensing: More information can be found here: https://github.com/akshathjain/grouped_buttons/blob/master/LICENSE
 */
 
 part of 'grouped_buttons.dart';
