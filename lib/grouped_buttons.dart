66398ebae6d67fa48988211818f66086e308903e
Tahmid <tahmid1989@gmail.com>
2020-04-14 04:27:42 -0500

    Added option to show radio text beside the button

--- lib/grouped_buttons.dart
+++ lib/grouped_buttons.dart
@@ -11,3 +11,4 @@ library grouped_buttons;
 export 'src/checkbox_group.dart';
 export 'src/radio_button_group.dart';
 export 'src/grouped_buttons_orientation.dart';
+export 'src/grouped_buttons_direction.dart';
---
+++ lib/src/grouped_buttons_direction.dart
@@ -0,0 +1,4 @@
+enum GroupedButtonDirection {
+  HORIZONTAL,
+  VERTICAL,
+}
